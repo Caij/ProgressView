25ef92cbb330ebf8aa23b67c0797232e6cf8d328
Caij <271945881@qq.com>
2016-10-21 12:59:48 +0800

    修改demo

--- app/src/main/java/com/caij/progerssview/app/MainActivity.java
+++ app/src/main/java/com/caij/progerssview/app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.caij.progerssview.app;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,8 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ProgressView progressView = (ProgressView) findViewById(R.id.pv);
+        final ProgressView progressView = (ProgressView) findViewById(R.id.pv);
         progressView.setMax(100);
-        progressView.setProgress(30);
+//        progressView.setProgress(30);
+        new Handler(){
+            int time = 0;
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                time ++;
+                progressView.setProgress(time);
+
+                sendEmptyMessageDelayed(1, 500);
+            }
+        }.sendEmptyMessage(1);
     }
 }
